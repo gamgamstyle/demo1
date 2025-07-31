268e90d600b74b9dd7bc4b4fd3a8b8e65f8d3994
gamgamstyle <gam@htl-villach.at>
2025-07-31 10:59:05 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // wichtig für Azure App Service
+  base: './',  // relative Pfade, perfekt für Azure App Service
 });
